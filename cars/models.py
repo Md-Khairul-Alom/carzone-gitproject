3a50a9eae19572a0bbcc6ec3476eda55be23e9c9
Md Khairul Alom <khairul9215@gmail.com>
2023-06-25 12:17:33 +0600

    search and admin panel

--- cars/models.py
+++ cars/models.py
@@ -89,6 +89,7 @@ class Car(models.Model):
     state=models.CharField(max_length=100)
     city=models.CharField(max_length=100)
     color=models.CharField(max_length=100)
+    # brand=models.CharField(max_length=100)
     model=models.CharField(max_length=100)
     year=models.IntegerField(('year'), choices=year_choice)
     condition=models.CharField(max_length=100)
